09de9691bf8c5eaa0cfb538c1e39c43f7cdd9232
IMO <180146096+IMOitself@users.noreply.github.com>
2026-03-13 08:11:08 +0800

    fix: pokemon images not loaded locally

--- resources/views/pokemon3.blade.php
+++ resources/views/pokemon3.blade.php
@@ -1,6 +1,6 @@
 <div style="justify-items: center; text-align: center; align-content: center; height: 100vh">
     <h1>Squirtle</h1>
-    <img src="https://pokedex.gg/api/image?url=https%3A%2F%2Futfs.io%2Ff%2Fa4c07669-905c-4284-8453-be378ba9f5a1-cx9vf7.png&w=96&format=webp&q=85">
+    <img src="{{ asset('pokemon3.webp') }}">
     
     <br><br><br>
     <a href="./pokemon1" style="font-size:x-large">
